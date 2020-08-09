34f20bac76fa72d6d671194a5b88eb9aebb72202
Anymo123 <1119885099@qq.com>
2020-08-09 09:23:52 +0800

    完成了权限功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,9 @@ import Login from '../components/login.vue'
 import Home from '../components/Home.vue'
 import Welcome from '../components/Welcome.vue'
 import Users from '../components/user/Users.vue'
+import Rights from '../components/power/Rights.vue'
+import Roles from '../components/power/Roles.vue'
+
 Vue.use(VueRouter)
 
 const routes = [{
@@ -22,6 +25,12 @@ const routes = [{
   }, {
     path: '/users',
     component: Users
+  }, {
+    path: '/rights',
+    component: Rights
+  }, {
+    path: '/roles',
+    component: Roles
   }]
 }]
 
